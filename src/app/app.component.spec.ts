fa3b76ac433cda1b5d5b1ec13720424583706412
kanakshukla-web <shukla.kanak7@gmail.com>
2021-04-01 16:39:42 +0530

    update build script

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MyCaseBuilder'`, () => {
+  it(`should have as title 'FoamBuilder'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MyCaseBuilder');
+    expect(app.title).toEqual('FoamBuilder');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('MyCaseBuilder app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('FoamBuilder app is running!');
   });
 });
